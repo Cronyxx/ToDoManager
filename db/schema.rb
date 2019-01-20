5faacf24ca57faa3b3b14fac2c9552ec929ae876
Cronyx <helloimyeotong@gmail.com>
2019-01-20 19:01:31 +0800

    Added Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_100402) do
+ActiveRecord::Schema.define(version: 2019_01_20_110042) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
